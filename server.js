815b49ebb0095eb952e83d7a9123de8822c53791
Pratik Vasagadekar <Vasagadekar.pratik@gmail.com>
2022-02-23 12:56:11 +0530

    First Push

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+const mongoose = require('mongoose')
+
+
+
+async function startServer()
+{
+    const app = express();
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app: app });
+    app.use((req, res) =>
+    {
+        res.send("Hello from Apollo Server");
+    });
+
+    await mongoose.connect('mongodb://localhost:27017/post_db', {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    console.log('Mongoose Connected')
+    app.listen(4000, () => console.log("Server Running on Port 4000"));
+}
+
+startServer();
+
+
+
